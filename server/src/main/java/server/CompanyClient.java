f1999dc3dc4c5b3074badb6cf1881eadea0c366b
Jeff Scott Brown <brownj@objectcomputing.com>
2018-10-19 13:51:25 -0500

    Use Micronaut 1.0.0.RC3

--- server/src/main/java/server/CompanyClient.java
+++ server/src/main/java/server/CompanyClient.java
@@ -4,7 +4,7 @@ import io.micronaut.http.HttpResponse;
 import io.micronaut.http.annotation.Delete;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.Post;
-import io.micronaut.http.client.Client;
+import io.micronaut.http.client.annotation.Client;
 import io.micronaut.retry.annotation.Retryable;
 
 import java.util.Map;
